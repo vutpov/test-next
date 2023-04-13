c8958fd9f5e5a3102c781aebd5c19c3c957aa614
Vut Pov <vuthpov26@gmail.com>
2023-04-13 16:18:02 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
+const withTwin = require('./withTwin.js')
+const withPWA = require('next-pwa')({
+    disable: process.env.NODE_ENV === 'development'
+});
+const { withPlugins } = require("next-compose-plugins");
+
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const plugins = [
+  [withPWA],
+]
+
+module.exports = withPlugins(plugins, withTwin(nextConfig))
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  theme: {
+    screens: {
+      xl: 1440,
+      lg: 1280,
+      md: 1024,
+      sm: 768,
+      xs: 480,
+    },
+  }
+}
